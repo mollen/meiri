bcbc3f5f24a6c444f86c90ea887d560194212c7b
Christopher Mollén <chris.mollen@gmail.com>
2024-03-24 10:57:35 +0100

    Add preschool level with subtraction

--- generate.py
+++ generate.py
@@ -7,14 +7,15 @@ import pypdf
 
 
 n_days = 10
+difficulty = 1
+tags = ["幼兒園", "學前班"]
+
+assert difficulty < len(tags)
 
 with open("base.tex", "r") as f_in:
     base = f_in.read()
 
-def get_number():
-    n = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-    p = np.array([1, 5, 4, 3, 2, 1, 2, 2, 1, 1], dtype=float)
-    p = p / np.sum(p)
+def get_number(n, p):
     number= np.random.choice(
       n,
       1,
@@ -22,32 +23,68 @@ def get_number():
     )
     return int(number)
 
-def make_exercise():
-    a = get_number()
-    b = get_number()
+def make_add_less_ten():
+    n = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    p = np.array([1, 5, 4, 3, 2, 1, 2, 2, 1, 1], dtype=float)
+    p = p / np.sum(p)
+    a = get_number(n , p)
+    b = get_number(n , p)
     while 10 < a + b:
-        a = get_number()
-        b = get_number()
+        a = get_number(n, p)
+        b = get_number(n, p)
  
     return f"{a} + {b} = "
 
-def make_exercises(n):
+def make_sub_greater_zero():
+    na = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    pa = np.array([1, 5, 4, 3, 2, 2, 2, 2, 1, 1], dtype=float)
+    pa = pa / np.sum(pa)
+
+    nb = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    pb = np.array([1, 5, 4, 3, 2, 2, 2, 2, 1, 1], dtype=float)
+    pb = pb / np.sum(pb)
+
+    a = get_number(na, pa)
+    b = get_number(nb, pb)
+    while a - b < 0:
+        a = get_number(na, pa)
+        b = get_number(nb, pb)
+
+    return f"{a} - {b} = "
+
+def make_add_sub_less_ten():
+    p_add = 0.7
+    exercise = ""
+    if np.random.uniform() < p_add:
+        exercise = make_add_less_ten()
+    else:
+        exercise = make_sub_greater_zero()
+    return exercise
+
+def make_exercise(difficulty):
+    exercise = ""
+    if difficulty == 0:
+        exercise = make_add_less_ten()
+    elif difficulty == 1:
+        exercise = make_add_sub_less_ten()
+    return exercise
+
+def make_exercises(n, difficulty):
     exercises = []
     for _ in range(n):
-        exercise = make_exercise()
+        exercise = make_exercise(difficulty)
         while exercise in exercises:
-            exercise = make_exercise()
+            exercise = make_exercise(difficulty)
         exercises.append(exercise)
     return exercises
 
 for n in range(n_days):
     content = base
 
-    content = content.replace("§級別", "學前班")
+    content = content.replace("§級別", tags[difficulty])
 
-    exercises = make_exercises(30)
+    exercises = make_exercises(30, difficulty)
     for i, exercise in enumerate(exercises):
-        excerize = make_exercise()
         content = content.replace(f"§{i+1:02}", exercise)
 
     with open(f"meiri{n}.tex", "w") as f_out:
@@ -64,5 +101,5 @@ for i in range(n_days):
     for f in old_files:
         os.remove(f)
 
-merger.write("meiri.pdf")
+merger.write(f"{tags[difficulty]}.pdf")
 merger.close()
